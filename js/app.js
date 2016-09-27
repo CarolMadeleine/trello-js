7ad1ae5af881621b19c591d5404085eeb0d4250e
CarolMadeleine <karitomadeleine@gmail.com>
2016-09-27 13:45:36 -0500

    subiendo cambios de la version 4

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ window.addEventListener("load", function(){
     var lista = document.getElementById("lista");
     var añadir = document.getElementById("añadir");
     var botonLista = document.getElementById("botonLista");
+    var añadirTarjeta = document.getElementById("añadirTarjeta");
 
     lista.addEventListener("click", function(){
         lista.classList.add("sevaya");
@@ -21,16 +22,42 @@ window.addEventListener("load", function(){
     listaTrello.insertBefore(mensajeTrello, listaTrello.childNodes[0]);
 
     var añadirTarjeta= document.createElement("a");
+    añadirTarjeta.setAttribute("id", "añadirTarjeta");
     añadirTarjeta.classList.add("añadirTarjeta");
     listaTrello.insertBefore(añadirTarjeta, listaTrello.childNodes[1]);
     var textoTarjeta = document.createTextNode("Añadir una Tarjeta...");
     añadirTarjeta.appendChild(textoTarjeta);
 
-  lista.classList.add("aparecer2");
+    lista.classList.add("aparecer2");
+
+    añadirTarjeta.addEventListener("click", function(e){
+
+    e.preventDefault();
+    añadirTarjeta.classList.add("tarjeDesaparecer");
+
+    var boxArea= document.createElement("form");
+        boxArea.classList.add("boxArea");
+        listaTrello.insertBefore(boxArea, listaTrello.childNodes[1]);
+
+    var iconoLista= document.createElement("span");
+        iconoLista.classList.add("icon-cross");
+        boxArea.insertBefore(iconoLista, boxArea.childNodes[2]);
+
+    var botonLista = document.createElement("button");
+        botonLista.classList.add("botonLista");
+        boxArea.insertBefore(botonLista, boxArea.childNodes[1]);
+        var textoBoton = document.createTextNode("Añadir");
+        botonLista.appendChild(textoBoton);
+
+    var textArea= document.createElement("textarea");
+        textArea.classList.add("textArea");
+        boxArea.insertBefore(textArea, boxArea.childNodes[0]);
+
 
 });
 
 
+});
 
 
 });
