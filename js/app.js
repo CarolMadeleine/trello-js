b79b95af86afa07ba2390812f2394eedb8b5a249
CarolMadeleine <karitomadeleine@gmail.com>
2016-09-27 12:44:10 -0500

    subiendo cambios js

--- js/app.js
+++ js/app.js
@@ -1,60 +1,35 @@
 window.addEventListener("load", function(){
+    var listaTrello = document.getElementById("listaTrello");
     var lista = document.getElementById("lista");
-    var contenedorTrello = document.getElementById("contenedorTrello");
-    // añadirLista();
+    var añadir = document.getElementById("añadir");
+    var botonLista = document.getElementById("botonLista");
 
-/*function añadirLista(){
-	var caja = document.createElement("div");
-      caja.classList.add("listaTrello");
-      lista.insertBefore(caja, lista.childNodes[0]);
-      var texto = document.createTextNode("Añadir una Lista ...");
-      caja.appendChild(texto);
-    } para poder añadir un div con texto*/
+    lista.addEventListener("click", function(){
+        lista.classList.add("sevaya");
+        añadir.classList.add("aparecer");
 
-lista.addEventListener("click", function(){
+});
 
-	lista.classList.add("sevaya");
+  botonLista.addEventListener("click", function(e){
 
-	var añadir = document.createElement("form");
-      añadir.classList.add("listaTrello");
-      contenedorTrello.insertBefore(añadir, contenedorTrello.childNodes[0]);
+    e.preventDefault();
+    añadir.classList.add("form2");
 
-  var iconoLista= document.createElement("span");
-      iconoLista.classList.add("icon-cross");
-      añadir.insertBefore(iconoLista, añadir.childNodes[2]);
-
-  var botonLista = document.createElement("div");
-      botonLista.classList.add("botonLista");
-      añadir.insertBefore(botonLista, añadir.childNodes[1]);
-      var textoBoton = document.createTextNode("Guardar");
-      botonLista.appendChild(textoBoton);
-
-  var inputLista= document.createElement("input");
-      inputLista.classList.add("inputLista");
-      inputLista.setAttribute("placeholder", "Añadir una lista ...");
-      añadir.insertBefore(inputLista, añadir.childNodes[0]);
-	
-	botonLista.addEventListener("click", function(e){
-
-		e.preventDefault();
-		añadir.classList.add("form2");
-
-		var mensajeTrello = document.createElement("div")
-		mensajeTrello.classList.add("mensajeTrello");
+    var mensajeTrello = document.createElement("div")
+    mensajeTrello.classList.add("mensajeTrello");
     mensajeTrello.innerText = inputLista.value;
-    contenedorTrello.insertBefore(mensajeTrello, contenedorTrello.childNodes[0]);
+    listaTrello.insertBefore(mensajeTrello, listaTrello.childNodes[0]);
 
     var añadirTarjeta= document.createElement("a");
     añadirTarjeta.classList.add("añadirTarjeta");
-    contenedorTrello.insertBefore(añadirTarjeta, contenedorTrello.childNodes[1]);
+    listaTrello.insertBefore(añadirTarjeta, listaTrello.childNodes[1]);
     var textoTarjeta = document.createTextNode("Añadir una Tarjeta...");
     añadirTarjeta.appendChild(textoTarjeta);
 
-
-
-	});
+  lista.classList.add("aparecer2");
 
 });
+
 
 
 
